70b237281a0c7101f063b77abebbf992978ec572
Daniel Williams <t15@yougothacked.org>
2016-04-26 14:02:01 +0100

    Exercise 7: More Printing
    Added in the study drills as a change.

--- Exercise 7: More Printing.py
+++ Exercise 7: More Printing.py
@@ -1,21 +1,31 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10  # what'd that do?
+print "Mary had a little lamb." # Prints "Mary had a little lamb."
+print "It's fleece was white as %s." % 'snow' # Prints "It's fleece was white as snow."
+print "And everywhere that Mary went." # Prints "And everywhere that Mary went."
+print "." * 10 # What'd that do? I'm sure it prints full stop 10 times.
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+end1 = "C" # Makes the variable end1 "C"
+end2 = "h" # Makes the variable end2 "h"
+end3 = "e" # Makes the variable end3 "e"
+end4 = "e" # Makes the variable end4 "e"
+end5 = "s" # Makes the variable end5 "s"
+end6 = "e" # Makes the variable end6 "e"
+end7 = "B" # Makes the variable end7 "B"
+end8 = "u" # Makes the variable end8 "u"
+end9 = "r" # Makes the variable end9 "r"
+end10 = "g" # Makes the variable end10 "g"
+end11 = "e" # Makes the variable end11 "e"
+end12 = "r" # Makes the variable end12 "r"
 
-# watch that comma at the end.  try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+# Watch that comma at the end, try removing it and seeing what it does!
+# Prediction = Moves it to a new line...
+# Edit -  I was wrong, turns out it seperated it, removing the comma moves it to a new line.
+
+print end1 + end2 + end3 + end4 + end5 + end6, # Prints the variables 1 - 6, which will be "Cheese"
+print end7 + end8 + end9 + end10 + end11 + end12 # Prints the variables 7 - 12, which will be "Burger"
+
+# === STUDY DRILLS ===
+# 1. Go back through and write a comment on what each line does.
+# 2. Read each one backward or out loud to find your errors.
+# 3. From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# 4. When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# 5. Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
